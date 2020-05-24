e32ef0ead4637aa21199a7a8dce6409ce0bf6dcc
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-05-24 14:34:54 -0400

    Load budget and register data

--- load.R
+++ load.R
@@ -5,4 +5,11 @@ library(janitor)
 
 library(helpers)
 
-
+budget <- read_csv("data/source/budget.csv") %>% clean_names
+register <- read_csv("data/source/register.csv") %>%
+  clean_names %>%
+  mutate_at(
+    c("outflow", "inflow"),
+    ~ as.numeric(str_remove_all(.x, "[^0-9\\.]"))
+  ) %>%
+  mutate(spend = inflow - outflow)
